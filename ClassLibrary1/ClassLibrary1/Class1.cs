851ac187870a1ae59dfe8a29a719cfdb7d6dd5e1
Anna <anlok.x3@gmail.com>
2023-04-19 19:07:32 +0700

    1

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funco
+{
+    public class Class1
+    {
+        public static void SumFunc(string x, string y)
+        {
+            var a = x.Split(',');
+            int sum = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                sum = Int32.Parse(a[i]) + sum;
+            }
+
+            var b = y.Split(',');
+            int mult = 1;
+
+            for (int i = 0; i < b.Length; i++)
+            {
+                mult = Int32.Parse(b[i]) * mult;
+            }
+            Console.WriteLine(sum);
+            Console.WriteLine(mult);
+        }
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Funco;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа через пробел:");
+            var a = Console.ReadLine();
+            var param = a.Split(' ');
+            var i = param.Length/2;
+            string[] firstHalf = param.Take(i).ToArray();
+            string[] secondHalf = param.Skip(i).ToArray();
+
+            Class1.SumFunc(String.Join(", ", firstHalf), String.Join(", ", secondHalf));
+        }
+    }
+}
